716bad61aa9ac4db9a7f26c7ecfd8e6c1890e52e
yazloysasha <yazloysasha@gmail.com>
2024-11-15 19:41:27 +0700

    Added viewing of map in pieces

--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,24 +1,80 @@
+import {
+  TransformWrapper,
+  TransformComponent,
+  type ReactZoomPanPinchRef,
+} from "react-zoom-pan-pinch";
 import Image from "next/image";
 import styles from "./styles.module.scss";
 import { TILE_SIZE } from "../../shared/consts";
+import { useState, type ReactNode } from "react";
 import type { IManifest, Tile } from "../../shared/types";
-import { useState, useEffect, type ReactNode } from "react";
-import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
 
 export default function Map({ manifest }: { manifest: IManifest }): ReactNode {
   const [shownTiles, setShownTiles] = useState<Tile[][]>([]);
 
-  useEffect(() => {
-    const tiles: Tile[][] = [[]];
+  const update = (
+    scale: number,
+    positionX: number,
+    positionZ: number
+  ): void => {
+    positionX /= scale;
+    positionZ /= scale;
+    positionX += manifest.indent.x;
+    positionZ += manifest.indent.z;
+    positionX *= -1;
+    positionZ *= -1;
 
-    for (let x = -4; x <= 4; x++) {
-      for (let z = -4; z <= 4; z++) {
-        tiles[0].push([x, z]);
-      }
-    }
+    let rightX = positionX + window.innerWidth / scale;
+    let bottomZ = positionZ + window.innerHeight / scale;
 
-    setShownTiles(tiles);
-  }, []);
+    const indent = TILE_SIZE / scale;
+
+    positionX -= indent;
+    positionZ -= indent;
+    rightX += indent;
+    bottomZ += indent;
+
+    const newShownTiles: Tile[][] = manifest.tiles
+      .filter((_, y) => y === 0)
+      .map((layer, y) => {
+        const size = (y + 1) * TILE_SIZE;
+
+        return layer.filter(([x, z]) => {
+          const startX = x * size;
+          const startZ = z * size;
+          const endX = startX + size;
+          const endZ = startZ + size;
+
+          return (
+            positionX <= startX &&
+            startX < rightX &&
+            positionX < endX &&
+            endX <= rightX &&
+            positionZ <= startZ &&
+            startZ < bottomZ &&
+            positionZ < endZ &&
+            endZ <= bottomZ
+          );
+        });
+      });
+
+    setShownTiles(newShownTiles);
+  };
+
+  const deferredUpdate = (event: ReactZoomPanPinchRef): void => {
+    const uuid = crypto.randomUUID();
+
+    localStorage.setItem("uuid", uuid);
+
+    setTimeout(() => {
+      const savedUuid = localStorage.getItem("uuid");
+      if (uuid !== savedUuid) return;
+
+      const { scale, positionX, positionY } = event.state;
+
+      update(scale, positionX, positionY);
+    }, 100);
+  };
 
   return (
     <TransformWrapper
@@ -26,6 +82,9 @@ export default function Map({ manifest }: { manifest: IManifest }): ReactNode {
       minScale={0.015}
       maxScale={2}
       centerOnInit
+      onZoom={deferredUpdate}
+      onPanning={deferredUpdate}
+      onInit={deferredUpdate}
     >
       <TransformComponent>
         <div className={styles.map} style={manifest.sizes}>
@@ -41,8 +100,8 @@ export default function Map({ manifest }: { manifest: IManifest }): ReactNode {
                 width={size}
                 height={size}
                 style={{
-                  left: manifest.indent.x + x * size - x * y,
-                  top: manifest.indent.z + z * size - z * y,
+                  left: manifest.indent.x + x * size,
+                  top: manifest.indent.z + z * size,
                   transform: `scale(${scale})`,
                 }}
               />
