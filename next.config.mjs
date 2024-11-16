6add4a7b2812d6b0a817e3edfcb43c08102ae9b6
yazloysasha <yazloysasha@gmail.com>
2024-11-16 09:43:44 +0700

    Fixed timeout (set to max 32-bit signed integer)

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   publicRuntimeConfig: {
     TITLE: process.env.TITLE,
   },
-  staticPageGenerationTimeout: 1000 * 60 * 60 * 24 * 365,
+  staticPageGenerationTimeout: 2147483647,
 };
 
 export default nextConfig;
