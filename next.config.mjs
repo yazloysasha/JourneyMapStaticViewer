2a7a6ce0f6f6b806beb6c9335323530541df9d00
yazloysasha <yazloysasha@gmail.com>
2024-11-16 09:49:41 +0700

    Fixed timeout !!!!!!!

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
   publicRuntimeConfig: {
     TITLE: process.env.TITLE,
   },
-  staticPageGenerationTimeout: 60 * 60 * 24 * 30,
+  staticPageGenerationTimeout: 60 * 60 * 24 * 7,
 };
 
 export default nextConfig;
